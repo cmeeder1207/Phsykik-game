7a448b3f1e8a3c3b926ce0fa74b8e7e20dad21a4
Chris Meeder <cmeeder1207@gmail.com>
2018-08-02 17:17:45 -0700

    still cant get the ui to run. gonna slap jqurey on it and see if that helps. then bootstrap at the end to sytle it up a bit.

--- main.js
+++ main.js
@@ -7,32 +7,95 @@ var wins = 0;
 var losses = 0;
 var guesses = 10;
 
-    var computerChoice = alphabet[Math.random(Math.floor() * alphabet.length)];
-
-    console.log(computerChoice)
-
+   
 
 
    
-   document.onkeypress = function(event) {
+   document.onkeyup = function(event) {
     var userGuess = event.key;
+ var computerChoice = alphabet[Math.floor(Math.random() * alphabet.length)];
+
+    
 
     if(userGuess === computerChoice){
-        w++;
+        wins++;
+        updateWins
     }else{
-        g--;
+        guesses--;
+        updateGuesses
     }
 
     if(guesses === 0){
-        l++
-        g=10
+        losses++
+        guesses=10
+        updateLosses
     }
 
- 
-    console.log (wins)
-    console.log(losses)
-    console.log(guesses)
+    if(losses === 0 && guesses===1){
+      var whatskeepingyou = prompt("What is Keeping you Here?")
+    }
+    if(losses===1 && guesses===5){
+       alert("yOU kEeP Going iN CiClEs");
+       alert(whatskeepingyou);
+
+    }
+
+    if (losses===2&& guesses===7){
+        alert("JUST");
+        alert("GIVE");
+        alert("UP");
+    }
+    if (losses===2&& guesses===6){
+        alert(whatskeepingyou);
+        alert("yOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEs jUST GIVE upyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEs");
+        alert("yOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEs jUST GIVE upyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEs");
+        alert("JUST");
+        alert("GIVE");
+        alert("UP");
+        alert("yOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEs jUST GIVE upyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEs");
+        alert(whatskeepingyou);
+        alert("yOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEs jUST GIVE upyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEs");
+        alert("yOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEs jUST GIVE upyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEs");
+        alert("yOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEs jUST GIVE upyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEsyOU kEeP Going iN CiClEs");
+        alert(whatskeepingyou);
+        alert(whatskeepingyou);
+        alert("JUST");
+        alert("GIVE");
+        alert("UP");
+        alert("JUST");
+        alert("GIVE");
+        alert("UP");
+    }
+    if (losses>=2 && guesses<6){
+        alert("game over");
+        alert("game over");
+        alert("game over");
+        alert("game over");
+        alert("game over");
+        alert("game over");
+        alert("game over");
+        alert("game over");
+        alert("game over");
+        alert("game over");
+        alert("game over");
+        alert("game over");
+        alert("game over");
+        alert("game over");
+        alert("game over");
+        alert("game over");
+        alert("game over");
+        alert("game over");
+        alert("game over");
+        alert("game over");
+
+        
+        
+    }
+  
+    console.log("Your Choice " + userGuess)
+    console.log("Computers Choice " + computerChoice)
+    console.log ("Wins:" + wins)
+    console.log("Losses: " + losses)
+    console.log("Guesses: " + guesses)
 } 
-wins.createElement('h1')
-losses.createElement('h1')
-guesses.createElement('h1')
+
